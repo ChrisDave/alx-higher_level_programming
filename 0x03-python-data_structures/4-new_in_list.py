cfdc2b817beb0a42f960b5ac78f5a0ea35439b96
ChrisDave <daveokai19@gmail.com>
2022-09-26 22:20:15 +0000

    assignement operator added

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
     if (idx < 0) or (idx > (len(my_list) - 1):
         return my_list
 
-    copy_list [n for n in my_list]
+    copy_list = [n for n in my_list]
     copy_list[idx] = element
     return copy_list
